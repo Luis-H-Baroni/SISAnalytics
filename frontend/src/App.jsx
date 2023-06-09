6bcc045c0ed2af884d715e896ecfe50c56923e50
Isabela Maria <isabeladacampo@gmail.com>
2023-06-09 18:12:48 -0300

    feat: missing files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import 'react-toastify/dist/ReactToastify.css';
